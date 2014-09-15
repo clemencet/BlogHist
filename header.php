fe47e5245e07669b9dc03700234f096859a2ba27
clemencet <clemence.m.thomas@gmail.com>
2014-09-15 12:36:07 +0200

    php
    Passage à PHP

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blog Histoire de M. Minaudier</title>
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Habibi' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- Leave those next 4 lines if you care about users using IE8 -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="margin">
+	<header>
+		<h1>Je vous ai compris !</h1>
+		<img src="images/lhooq.jpg">
+		<h2>L'Histoire autrement</h2>
+		<nav>
+			<ul>
+				<li><a href="#">Reines, Rois et dirigeants</a></li>
+				<li><a href="#">Batailles</a></li>
+				<li><a href="#">L'Inconnu du coin</a></li>
+				<li><a href="#">Le quiz du héros</a></li>
+			</ul>
+		</nav>
+		<div id="margin">
+	</header>	
